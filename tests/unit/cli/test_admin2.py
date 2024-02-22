fcfd864524e5354c371e579a6f950547125d6692
Lukas Puehringer <lukas.puehringer@nyu.edu>
2024-02-22 15:26:44 +0100

    Quick and dirty admin2 cli payload test
    - add payload fixtures for ceremony, update, sign
    - generate payload in test run and write to tmp file
      (use isolated file)
    - do basic comparisons of payload and expected payload
      - strip undeterministic sigs and expiration date updates
    
    - adds unrelated fix to load test file paths using absolute path
    
    Signed-off-by: Lukas Puehringer <lukas.puehringer@nyu.edu>

--- tests/unit/cli/test_admin2.py
+++ tests/unit/cli/test_admin2.py
@@ -1,3 +1,4 @@
+import json
 from pathlib import Path
 
 import pretend
@@ -11,6 +12,7 @@ from repository_service_tuf.cli.admin2 import ceremony, sign, update
 _FILES = Path(__file__).parent.parent.parent / "files"
 _ROOTS = _FILES / "root"
 _PEMS = _FILES / "pem"
+_PAYLOADS = _FILES / "payload"
 
 
 @pytest.fixture
@@ -61,10 +63,32 @@ class TestAdmin2:
             "1",  # Please enter '<number>' to choose a signing key, or press enter to continue:
             f"{_PEMS / 'ec'}",  # Please enter path to encrypted local private key
         ]
-        result = client.invoke(
-            ceremony, input="\n".join(inputs), catch_exceptions=False
-        )
-        print(result.output)
+
+        with client.isolated_filesystem():
+            result = client.invoke(
+                ceremony,
+                args=["-o", "payload.json"],
+                input="\n".join(inputs),
+                catch_exceptions=False,
+            )
+            print(result.output)
+
+            with open("payload.json") as f:
+                payload = json.load(f)
+
+        with open(_PAYLOADS / "ceremony-payload.json") as f:
+            expected_payload = json.load(f)
+
+        payload["metadata"]["root"]["signed"].pop("expires")
+        expected_payload["metadata"]["root"]["signed"].pop("expires")
+
+        sigs = payload["metadata"]["root"].pop("signatures")
+        expected_sigs = expected_payload["metadata"]["root"].pop("signatures")
+
+        assert [sig["keyid"] for sig in sigs] == [
+            sig["keyid"] for sig in expected_sigs
+        ]
+        assert payload == expected_payload
 
     def test_update(self, client, patch_getpass):
         inputs = [
@@ -73,35 +97,73 @@ class TestAdmin2:
             "1",  # Please enter root threshold
             "2",  # Please press '0' to add key, or enter '<number>' to remove key. Press enter to continue
             "1",  # Please press '0' to add key, or enter '<number>' to remove key. Press enter to continue
-            "tests/files/pem/rsa.pub",  # Please enter a public key path
+            f"{_PEMS / 'rsa.pub'}",  # Please enter a public key path
             "rsa root key",  # Please enter a key name
             "",  # Please press '0' to add key, or enter '<number>' to remove key. Press enter to continue:
             "y",  # Do you want to change the online key? [y/n] (y)
-            "tests/files/pem/ec.pub",  # Please enter a public key path
+            f"{_PEMS / 'ec.pub'}",  # Please enter a public key path
             "1",  # Please enter '<number>' to choose a signing key
-            "tests/files/pem/ed",  # Please enter path to encrypted local private key
+            f"{_PEMS / 'ed'}",  # Please enter path to encrypted local private key
             "1",  # Please enter '<number>' to choose a signing key
-            "tests/files/pem/ec",  # Please enter path to encrypted local private key
+            f"{_PEMS / 'ec'}",  # Please enter path to encrypted local private key
             "1",  # Please enter '<number>' to choose a signing key
-            "tests/files/pem/rsa",  # Please enter path to encrypted local private key
+            f"{_PEMS / 'rsa'}",  # Please enter path to encrypted local private key
         ]
-        result = client.invoke(
-            update,
-            args=[f"{_ROOTS / 'v1.json'}"],
-            input="\n".join(inputs),
-            catch_exceptions=False,
-        )
-        print(result.output)
+
+        with client.isolated_filesystem():
+            result = client.invoke(
+                update,
+                args=[f"{_ROOTS / 'v1.json'}", "-o", "payload.json"],
+                input="\n".join(inputs),
+                catch_exceptions=False,
+            )
+            print(result.output)
+
+            with open("payload.json") as f:
+                payload = json.load(f)
+
+        with open(_PAYLOADS / "update-payload.json") as f:
+            expected_payload = json.load(f)
+
+        payload["metadata"]["root"]["signed"].pop("expires")
+        expected_payload["metadata"]["root"]["signed"].pop("expires")
+
+        sigs = payload["metadata"]["root"].pop("signatures")
+        expected_sigs = expected_payload["metadata"]["root"].pop("signatures")
+
+        assert [sig["keyid"] for sig in sigs] == [
+            sig["keyid"] for sig in expected_sigs
+        ]
+        assert payload == expected_payload
 
     def test_sign(self, client, patch_getpass):
         inputs = [
             "4",  # Please enter '<number>' to choose a signing key:
-            "tests/files/pem/rsa",  # Please enter path to encrypted local private key:
+            f"{_PEMS / 'rsa'}",  # Please enter path to encrypted local private key:
         ]
-        result = client.invoke(
-            sign,
-            args=[f"{_ROOTS / 'v2.json'}", f"{_ROOTS / 'v1.json'}"],
-            input="\n".join(inputs),
-            catch_exceptions=False,
+
+        with client.isolated_filesystem():
+            result = client.invoke(
+                sign,
+                args=[
+                    f"{_ROOTS / 'v2.json'}",
+                    f"{_ROOTS / 'v1.json'}",
+                    "-o",
+                    "payload.json",
+                ],
+                input="\n".join(inputs),
+                catch_exceptions=False,
+            )
+            print(result.output)
+
+            with open("payload.json") as f:
+                payload = json.load(f)
+
+        with open(_PAYLOADS / "sign-payload.json") as f:
+            expected_payload = json.load(f)
+
+        assert payload["role"] == "root"
+        assert (
+            payload["signature"]["keyid"]
+            == expected_payload["signature"]["keyid"]
         )
-        print(result.output)
